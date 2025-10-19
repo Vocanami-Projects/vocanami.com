22a1fd293260882e40674e77587189d8b2122915
DESKTOP-G1N3UP3\Owner <jd.sciftech.to@gmail.com>
2025-10-19 15:34:48 +0900

    Fixed

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -12,4 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
             document.querySelector("footer").innerHTML = data;
         });
 
+    const includeSections = document.querySelectorAll('[data-include]');
+    
+    includeSections.forEach(el => {
+        const filePath = el.getAttribute('data-include');
+        fetch(filePath)
+            .then(response => response.text())
+            .then(data => {
+                el.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Error loading section:', filePath, error);
+            });
+    });
+
 });
